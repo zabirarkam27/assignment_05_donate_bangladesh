28a9e6d3c00e970852a80a56b310ff2e20876bab
Zabir Arkam <zabirarkam27@gmail.com>
2024-12-06 22:43:51 +0600

    toggle page added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const btnDonation = document.getElementById("btn-donation");
 const btnHistory = document.getElementById("btn-history");
+const historyPage = document.getElementById("history-page");
+const donationPage = document.getElementById("donation-page");
+
 
 // Function for Changing Active btn style
 function setActiveBtn(activeBtn, inactiveBtn) {
@@ -9,9 +12,18 @@ function setActiveBtn(activeBtn, inactiveBtn) {
     inactiveBtn.classList.remove("bg-primary", "border-primary");
     inactiveBtn.classList.add("bg-white", "text-textColor");
 }
+
+// Function for toggle pages
+function togglePages(activePage, hiddenPage) {
+    activePage.classList.remove("hidden");
+    hiddenPage.classList.add("hidden");
+}
+
 btnDonation.addEventListener("click", function() {
     setActiveBtn(btnDonation, btnHistory);
+    togglePages(donationPage, historyPage);
 })
 btnHistory.addEventListener("click", function() {
     setActiveBtn(btnHistory, btnDonation);
+    togglePages(historyPage, donationPage);
 })
